ee0729251d01f0f59b97be8c456254381ff628e2
Yashvi017 <yashvitrivedi17@gmail.com>
2025-06-20 16:36:52 +0530

    project admin panel template and dashboard done

--- Reactjs/App1/app1/src/index.js
+++ Reactjs/App1/app1/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 
+https://dm.1024terabox.com/main?category=all&path=%2F&fsid=303848541291535&isMultiTransfer=0
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render();
